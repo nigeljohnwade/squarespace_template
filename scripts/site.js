e69443fb0f006001b4e04e82da6cca3b8106ffcb
Squarespace, Inc. <noreply@squarespace.com>
2013-12-13 12:15:52 +0000

    assets/readme.md blocks/navigation.block collections/blog.conf collections/blog.item collections/blog.list collections/folders.conf pages/readme.md scripts/site.js site.region styles/base.less styles/reset.css template.conf

---
+++ scripts/site.js
@@ -0,0 +1,25 @@
+/*
+
+	Feel free to load up JQuery in your site.region.
+
+	We use YUI3 and you can, too. For JQuery developers,
+	a helpful guide to learning YUI3 can be found at:
+
+	http://www.jsrosettastone.com/
+
+*/
+
+Y.use('node', function(Y) {
+	Y.on('domready', function() {
+
+		// Do stuff here.
+
+	});
+});
+
+/*
+
+	See the developer docs for JavaScript examples.
+	http://developers.squarespace.com/custom-javascript/
+
+*/
